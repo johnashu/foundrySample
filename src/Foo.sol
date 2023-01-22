ef4e854de2de651f3b679f98fceff9f3e353b25b
Maffaz <johnashu1@hotmail.com>
2023-01-22 01:05:07 +0100

    [ADD] test

--- src/Foo.sol
+++ src/Foo.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract Foo {
+contract Foo is Ownable {
     function bar() view external {
         require(msg.sender == address(1), 'wrong address');
     }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
-
 contract CounterTest is Test {
     Counter public counter;
+
+    uint testNumber = 42;
 
     address internal madeUp;
 
@@ -26,6 +27,12 @@ contract CounterTest is Test {
         assertEq(counter.number(), x);
     }
 
+    function testSetNumberRevert(uint256 x) public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.setNumber(type(uint256).max + 1);
+        assertEq(counter.number(), x);
+    }
+
     function testIsTrue() public {
         emit log_string("\nHello\n\tThis is gonna be True!!\nSent by:");
         emit log_address(msg.sender);
@@ -33,11 +40,21 @@ contract CounterTest is Test {
         require(counter.isTrue(), "It is False!");
     }
 
-    function testAddOne() public {
-        assertEq(3, counter.addOne(2));
+    function testCannotSubtract43() public {
+        vm.expectRevert(stdError.arithmeticError);
+        testNumber -= 43;
     }
 
-    // function testAddOneFuzzy(uint256 x) public {
-    //     assertEq(x + 1, counter.addOne(x));
-    // }
+    function testAddOne() public {
+        assertEq(counter.addOne(2), 3);
+    }
+
+    function testFailAddOne(uint x) public {
+        assertEq(counter.addOne(x), x + 2);
+    }
+
+    function testAddOneFuzzy(uint256 x) public {
+        vm.assume(x < type(uint256).max - 1);
+        assertEq(counter.addOne(x), x + 1);
+    }
 }
